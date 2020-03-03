62c8405a5bf2f02bfedc1bbd592c71337137e636
Hai Nghiem <hainghiem375@gmail.com>
2020-03-03 13:28:59 -0330

    simple css & separated js from html

---
+++ map-stuff.js
@@ -0,0 +1,12 @@
+//Map component
+var nlmap = L.map('map').setView([48.56024979174329, -55.92041015625], 6);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoidGhhbmdoYWltZW93IiwiYSI6ImNrN2MydzFzeTAwNDQzZW1jZDEzNjhwb2QifQ.nD9ibuY2wFeARFb97sgleA'
+}).addTo(nlmap);
+
