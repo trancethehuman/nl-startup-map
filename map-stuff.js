043253fda27973c6ffafa67b36647d6e50052a3d
Hai Nghiem <hainghiem375@gmail.com>
2020-03-10 12:08:15 -0230

    Marker placement test

--- map-stuff.js
+++ map-stuff.js
@@ -1,6 +1,7 @@
 //Map component
 var nlmap = L.map('map').setView([48.56024979174329, -55.92041015625], 6);
 
+//Map Layer
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -10,3 +11,5 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoidGhhbmdoYWltZW93IiwiYSI6ImNrN2MydzFzeTAwNDQzZW1jZDEzNjhwb2QifQ.nD9ibuY2wFeARFb97sgleA'
 }).addTo(nlmap);
 
+//Putting icons on map
+var marker = L.marker([47.30903424774781, -53.173828125]).addTo(nlmap);
